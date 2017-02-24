3a175f740eceb5c0932b226ee51d3496f322f00c
michaelzhang <heronalps@gmail.com>
2017-02-24 09:41:39 -0800

    Commit_0224

---
+++ src/com/ucsb/michaelzhang/ConfigChange.java
@@ -0,0 +1,7 @@
+package com.ucsb.michaelzhang;
+
+/**
+ * Created by michaelzhang on 2/23/17.
+ */
+public class ConfigChange extends LogEntry {
+}
--- src/com/ucsb/michaelzhang/Main.java
+++ src/com/ucsb/michaelzhang/Main.java
@@ -1,6 +1,12 @@
 package com.ucsb.michaelzhang;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+
+
 
     public static void main(String[] args) {
 	// write your code here
